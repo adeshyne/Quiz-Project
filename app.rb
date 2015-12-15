434f579e90dcec81ff039e7891a126cc469176ef
Ade Akintide <ade@abreezy.home>
2015-12-15 04:26:46 -0500

    second commit

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'sinatra'
+
+
+
+get '/index' do
+	erb :index
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/play' do
+	erb :play
+end
+
+get '/quiz' do
+	erb :quiz
+end
+
+get '/contact' do
+	erb :contact
+end
+
+get '/fun' do
+	erb :fun
+end
+
+get '/fun1' do
+	erb :fun1
+end
+
+get '/fun2' do
+	erb :fun2
+end
+
+get '/fun3' do
+	erb :fun3
+end
+
+get '/fun4' do
+	erb :fun4
+end
+
+get '/fun5' do
+	erb :fun5
+end
+
+get '/fun6' do
+	erb :fun6
+end
